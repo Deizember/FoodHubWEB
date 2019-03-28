ca6d99178b7ac9d5a992bda1f1bfc31a9fe87e64
Jean Dela Cruz <delacruzjean058@gmail.com>
2019-03-28 14:24:27 +0800

    With API

--- app.py
+++ app.py
@@ -8,25 +8,29 @@ app = Flask(__name__)
 @app.route('/landing')
 def landing():
     return render_template('landing.html')
-@app.route('/restaurantfound')
-def restaurantfound():
-    return render_template('restaurant-found.html')
 @app.route('/restaurant1')
 def restaurant1():
     return render_template('restaurant1.html')
-@app.route('/homepage1')
-def homepage1():
-    return render_template('homepage1.html')
-@app.route('/homepage2')
-def homepage2():
-    return render_template('homepage2.html')
 @app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template('register.html', form=form)
+    return render_template('register.html')
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if Customer.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = Customer(username = username)
 
-@app.route('/RestaurantReservation',methods=['GET','POST'])
-def RestaurantReservation():
-    return render_template('register-reservation.html')
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+@app.route("/login", methods=['GET','POST'])
+def login():
+    return render_template('login.html')
 
 if __name__=='__main__':
-    app.run(debug=True)   
+    app.run(debug=True)
