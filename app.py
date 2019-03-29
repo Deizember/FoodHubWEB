7aff41982e16f7318efaffabdd614eaa1eec003b
Jean Dela Cruz <delacruzjean058@gmail.com>
2019-03-29 16:08:17 +0800

    Update

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def restaurant1():
 @app.route("/register", methods=['GET','POST'])
 def register():
 
-    firstname = request.form['firstname', False]
-    lastname = request.form['lastname', False]
-    gender = request.form['gender', False]
-    email = request.form['email', False]
-    contact_number = request.form['contact_number', False]
-    username = request.form['username', False]
-    password = request.form['password', False]
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    gender = request.form['gender]
+    email = request.form['email']
+    contact_number = request.form['contact_number']
+    username = request.form['username']
+    password = request.form['password']
     
     register_url = request.post("https://fierce-scrubland-63107.herokuapp.com/owner/register?username"+ username+"&password="+ password+ "&firstname="+firstname+"&lastname="+ lastname+"&contact_number="+contact_number+"&gender="+gender)
     register_json = register_url.json()
