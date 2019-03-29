eaf5fc28194a28012a94295803df5e63c39358b3
jeandelacruz1004 <delacruzjean058@gmail.com>
2019-03-29 15:19:15 +0800

    Changes in Routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect
 
 
 
@@ -14,21 +14,22 @@ def restaurant1():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-
-    firstname = request.form['firstname', False]
-    lastname = request.form['lastname', False]
-    gender = request.form['gender', False]
-    email = request.form['email', False]
-    contact_number = request.form['contact_number', False]
-    username = request.form['username', False]
-    password = request.form['password', False]
+    firstname = request.form['firstname']
+    print firstname
+    lastname = request.form['lastname']
+    gender = request.form['gender']
+    email = request.form['email']
+    contact_number = request.form['contact_number']
+    username = request.form['username']
+    password = request.form['password']
     
     register_url = request.post("https://fierce-scrubland-63107.herokuapp.com/owner/register?username"+ username+"&password="+ password+ "&firstname="+firstname+"&lastname="+ lastname+"&contact_number="+contact_number+"&gender="+gender)
     register_json = register_url.json()
-    return render_template('landing.html', register_json=register_json)
+    return render_template('landing.html', register_json=register_json, form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+
     return render_template('login.html')
 
 if __name__=='__main__':
