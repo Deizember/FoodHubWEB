3ef3503ca60cc56db684f104a7c326f5b1e3a778
jeandelacruz1004 <delacruzjean058@gmail.com>
2019-04-03 02:29:56 +0800

    Updated Login

--- app.py
+++ app.py
@@ -1,36 +1,60 @@
-from flask import Flask, render_template, request, flash, url_for, redirect
-
-
-
+from flask import Flask, render_template, request, url_for, redirect
+from flask_bootstrap import Bootstrap
+from forms import RegistrationForm, LogInForm
+from flask_login import current_user
+from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
+Bootstrap (app)
 
-@app.route('/landing')
-def landing():
+# @app.route('/')
+# def landing():
+#     return render_template('landing.html')
+
+
+# @app.route('/restaurant1')
+# def restaurant1():
+#     return render_template('restaurant1.html')
+
+# @app.route("/register", methods=['GET','POST'])
+# def register(): 
+#     return render_template('landing.html')
+@app.route("/official")
+def official():
+    return render_template('official.html')
+
+# @app.route('/customer/login', methods=['GET','POST'])
+# def Customerlogin():
+#     if request.method == "POST":
+#         username = request.form['username'] 
+#         password = request.form['password'] 
+
+#         response = request.post("http://127.0.0.1:5000/owner/login",
+#         json = {"username":username, "password":password})
+#         print(response.text)
+       
+    # return render_template('official.html')
+
+@app.route('/', methods=['GET','POST'])
+def Ownerlogin():
+    print('wa kasulod')
+    if request.method == "POST":
+        print('sulod')
+        username = request.form['username'] 
+        password = request.form['password'] 
+
+        response = requests.post("http://127.0.0.1:5000/owner/login",
+        json={"username":username, "password":password}, )
+        print(response.text)
+        return redirect(url_for('official'))  
     return render_template('landing.html')
-@app.route('/restaurant1')
-def restaurant1():
-    return render_template('restaurant1.html')
+@app.route('/customerProfile')
+def CustomerProfile():
+    return render_template('customer-profile.html')
 
-@app.route("/register", methods=['GET','POST'])
-def register():
-    firstname = request.form['firstname']
-    print firstname
-    lastname = request.form['lastname']
-    gender = request.form['gender']
-    email = request.form['email']
-    contact_number = request.form['contact_number']
-    username = request.form['username']
-    password = request.form['password']
-    
-    register_url = request.post("https://fierce-scrubland-63107.herokuapp.com/owner/register?username"+ username+"&password="+ password+ "&firstname="+firstname+"&lastname="+ lastname+"&contact_number="+contact_number+"&gender="+gender)
-    register_json = register_url.json()
-    return render_template('landing.html', register_json=register_json, form=form)
-
-@app.route("/login", methods=['GET','POST'])
-def login():
-
-    return render_template('login.html')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,threaded=True, port=4000)   
+
+    
--- config.py
+++ config.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 app.config[''] = 'testfile'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5433/FoodHub'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:/5432/FoodHub'
 
-SECRET_KEY
+app.config['SECRET_KEY'] = 'any secret string'
 app.static_folder = 'static'
 
 db = SQLAlchemy(app)
