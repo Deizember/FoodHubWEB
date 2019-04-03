2f8616ad80393e440b089ee201fe5e5ed1eff68a
jeandelacruz1004 <delacruzjean058@gmail.com>
2019-04-03 17:24:09 +0800

    Database name updated!

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 app.config[''] = 'testfile'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:/5433/foodhub'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5433/foodhub'
+
 
 app.static_folder = 'static'
 
