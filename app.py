42e2e13fc8c3e5541f7ae16e1452b6f81fb54ae7
jeandelacruz1004 <delacruzjean058@gmail.com>
2019-04-15 19:46:49 +0800

    Identifier for login user

--- app.py
+++ app.py
@@ -22,35 +22,47 @@ def customerlanding():
 @app.route('/', methods=['GET','POST'])
 def Ownerlogin():
     print('wa kasulod')
-   
+    
     if request.method == "POST":
         
         print('sulod')
         username = request.form['username'] 
         password = request.form['password']
-        #user_get = requests.get("http://127.0.0.1:5000/users/" + username)
-        #User = user.verify_auth_token(username_or_token)
-
-        #if User.id == "owner":
-        #    if request.form['username'] == 'admin' and request.form['password'] == 'password':
-        #         return redirect(url_for('ownerlanding'  ))
+        check_user = requests.get("http://127.0.0.1:5000/owner"+"/"+ username)
+        print(check_user.status_code)
+        if check_user.status_code == 200:
+            response = requests.post("http://127.0.0.1:5000/owner/login",json={"username":username, "password":password}, )
+            print(response.status_code)
+            if response.status_code == 400:
+                print("Username or password is incorrect")  
                 
-        # elif user.id == "costumer":
-        response = requests.post("http://127.0.0.1:5000/customer/login",json={"username":username, "password":password}, )
-        print(response.status_code)
-        if response.status_code == 400:
-            print("Username or password is incorrect")
-            
-        elif response.status_code == 200:
-            return redirect(url_for('ownerlanding'  ))
+            elif response.status_code == 200:
+                return redirect(url_for('ownerlanding'  ))
+        elif check_user.status_code == 404:
+            response = requests.post("http://127.0.0.1:5000/customer/login",json={"username":username, "password":password}, )
+            print(response.status_code)
+            if response.status_code == 400:
+                print("Username or password is incorrect")
+                
+            elif response.status_code == 200:
+                return redirect(url_for('customerlanding'  ))
+            else:
+                print("user not found")
+
+
+
     return render_template('landing.html')
 
 
 #Routing for Restaurant profile
 
-@app.route('/restaurantprofile')
+@app.route('/restaurantprofile', methods=['GET'])
 def restaurantprofile():
-    return render_template('restaurantprofile.html')
+    owner = 1
+    restaurant = requests.get("http://127.0.0.1:5000/restaurant/")
+    restau_json=restaurant.json()
+    print(restau_json)
+    return render_template('restaurantprofile.html', i=restau_json)
 
 @app.route('/restaurant', methods =['GET', 'POST'])
 def addrestau():
@@ -62,30 +74,28 @@ def addrestau():
         restaurant_type = request.form['restaurant_type']
         bio = request.form['bio']
         locations = request.form['locations']
+        owner = 1
 
 
         response = requests.post("http://127.0.0.1:5000/restaurant/",
-        json={"restaurant_name":restaurant_name, "restaurant_type":restaurant_type, "bio":bio, "locations":locations}, )
+        json={"restaurant_name":restaurant_name, "restaurant_type":restaurant_type, "bio":bio, "locations":locations, "owner":owner}, )
         print(response.text)
         return redirect(url_for('restaurantprofile'))  
     return render_template('reservationrestau.html')
 
 @app.route("/displayrestau", methods =['GET', 'POST'])
 def displayrestau():
-    if request.method == "POST":
-        owner = 1
-        response = request.post("http://127.0.0.1:5000/restaurant/",
-        json={"restaurant_name":restaurant_name, "restaurant_type":restaurant_type, "bio":bio, "locations":locations, "owner":owner}, )
-        restau_json=restaurant.json()
-        return redirect(url_for('restaurantprofile'))
-
-    return render_template('reservationrestau.html', resto=restau_json)
+    owner = 1
+    restaurant = requests.get("http://127.0.0.1:5000/restaurant/")
+    restau_json=restaurant.json()
+    
+    return render_template('ownerlanding.html', i=restau_json)
 
 @app.route("/deleterestau", methods =['GET', 'POST', 'PULL'])
 def deleterestau():
     if request.method == "POST":
         owner = 1
-        response = request.post("http://127.0.0.1:5000/restaurant/",
+        response = requests.put("http://127.0.0.1:5000/restaurant/",
         json={"restaurant_name":restaurant_name, "restaurant_type":restaurant_type, "bio":bio, "locations":locations, "owner":1}, )
         restau_json=restaurant.json()
         return redirect(url_for('restaurantprofile'))
