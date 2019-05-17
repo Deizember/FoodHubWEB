fce06491f4d5ddeeee968bc099e95fa3fdd605c5
jeandelacruz1004 <delacruzjean058@gmail.com>
2019-05-17 14:55:29 +0800

    Updated!

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 
 if __name__=='__main__':
-    port =int(os.environ.get("PORT",9090))
+    port =int(os.environ.get("PORT",8003))
     server.run(host='0.0.0.0',port=port)   
--- web/routes.py
+++ web/routes.py
@@ -1,10 +1,21 @@
-from web import Flask, render_template, request, url_for, redirect,flash, server
+from web import Flask, render_template, request, url_for, redirect,flash, server, session
+from flask_login import logout_user
 import requests
+from datetime import timedelta
 
 #Routing for landing pages
 @server.route('/')
 def index():
     return render_template('landing.html')
+@server.route('/burger')
+def burger():
+    return render_template('burger.html')
+@server.route('/afritada')
+def afritada():
+    return render_template('afritada.html')
+@server.route('/buttons')
+def buttons():
+    return render_template('buttons.html')
 @server.route('/profile')
 def profile():
     return render_template('profile.html')
@@ -22,41 +33,50 @@ def ownerlanding():
 def customerlanding():
     return render_template('customerlanding.html')
 
+# @server.before_request
+# def make_session_permanent():
+#     session.permanent = True
+#     app.permanent_session_lifetime = timedelta(minutes=5)
+
+#Routing for Logout
+@server.route("/logout")
+def logout():
+    logout_user()
+    return redirect(somewhere)
+
+
 #Routing for Login
-@server.route('/login', methods=['GET','POST'])
+@server.route('/login', methods=['POST','GET'])
 def login():
     print('wa kasulod')
-    
     if request.method == "POST":
         
         print('sulod')
         username = request.form['username'] 
         password = request.form['password']
-        check_user = requests.get("http://127.0.0.1:5000/owner"+"/"+ username)
-        print(check_user.status_code)
-        if check_user.status_code == 200:
-            response = requests.post("http://127.0.0.1:5000/owner/login",json={"username":username, "password":password}, )
+        owner_type = request.form['owner_type']
+        # if check_user.status_code == 200:
+        if owner_type == "owner":
+            response = requests.post("foodhubapi.herokuapp.com/owner/login",json={"username":username, "password":password}, )
             print(response.status_code)
+            print("owner")
             if response.status_code == 400:
                 print("Username or password is incorrect")  
                 
-            elif response.status_code == 200:
+            else: 
                 return redirect(url_for('ownerlanding'  ))
-        elif check_user.status_code == 404:
-            response = requests.post("http://127.0.0.1:5000/customer/login",json={"username":username, "password":password}, )
+        elif owner_type == "Customer":
+            response = requests.post("foodhubapi.herokuapp.com/customer/login",json={"username":username, "password":password}, )
             print(response.status_code)
+            print("custom")
             if response.status_code == 400:
                 print("Username or password is incorrect")
                 
-            elif response.status_code == 200:
-                return redirect(url_for('customerlanding'  ))
             else:
+                return redirect(url_for('customerlanding'  ))
                 print("user not found")
 
-
-
     return render_template('landing.html')
-
 
 #Routing for Registration 
 
@@ -77,7 +97,7 @@ def Registration():
         print(user_type)
 
         if user_type == "owner":
-            response = requests.post("http://127.0.0.1:5000/owner/",json={"firstname":firstname, "lastname": lastname, "gender": gender,"contact_number": contact_number,"username":username,"email":email, "password":password} )
+            response = requests.post("https://guarded-mesa-80703.herokuapp.com/owner/",json={"firstname":firstname, "lastname": lastname, "gender": gender,"contact_number": contact_number,"username":username,"email":email, "password":password} )
             print(response.status_code)
             if response.status_code == 400:
                 print("Username or password is incorrect")
@@ -85,7 +105,7 @@ def Registration():
             else:
                 return redirect(url_for('ownerlanding' ))
         else:
-            response = requests.post("http://127.0.0.1:5000/customer/",json={"firstname":firstname, "lastname": lastname, "gender": gender,"contact_number": contact_number,"username":username,"email":email, "password":password} )
+            response = requests.post("foodhubapi.herokuapp.com/customer/",json={"firstname":firstname, "lastname": lastname, "gender": gender,"contact_number": contact_number,"username":username,"email":email, "password":password} )
             print(response.status_code)
             if response.status_code == 400:
                 print("Username or password is incorrect")
@@ -102,12 +122,12 @@ def Registration():
 @server.route('/tables', methods=['GET'])
 def tables():
     
-    restaurantget = requests.get("http://127.0.0.1:5000/restaurant/1")
+    restaurantget = requests.get("foodhubapi.herokuapp.com/restaurant/1")
     restau=restaurantget.json()
     print(restau)
     return render_template('tables.html', displayres=restau)
 
-@server.route('/restaurant', methods =['GET', 'POST'])
+@server.route('/tables/<string:name>', methods =['PUT'])
 def addrestau():
     print('wa kasulod')
     if request.method == "POST":
@@ -117,22 +137,27 @@ def addrestau():
         restaurant_type = request.form['restaurant_type']
         bio = request.form['bio']
         locations = request.form['locations']
+        owner=1
 
-
-        response = requests.post("http://127.0.0.1:5000/restaurant/",
+        response = requests.post("foodhubapi.herokuapp.com/restaurant/",
         json={"restaurant_name":restaurant_name, "restaurant_type":restaurant_type, "bio":bio, "locations":locations,"owner":owner}, )
         print(response.text)
         return redirect(url_for('tables'))  
     return render_template('ownerlanding.html')
 
 
-@server.route("/deleterestau", methods =['GET', 'POST', 'PULL'])
+@server.route("/tables<string:name>", methods =['DELETE'])
 def deleterestau():
       
     return render_template('tables.html')
 
+@server.route("/search", methods=['POST', 'GET'])
+def search():
+    url:'foodhubapi.herokuapp.com/restaurant'
 
-
-
-
+#Routing for Customer Profile
+@server.route('/customerProfile')
+#@login_required
+def customerProfile():
     
+    return render_template('customer-profile.html')
